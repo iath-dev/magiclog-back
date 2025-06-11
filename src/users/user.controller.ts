fbef45cd600360b5f3acce1370c5d5dba2e31376
Daniel Develop <57878266+iath-dev@users.noreply.github.com>
2025-06-10 19:06:32 -0500

    refactor: remove unused user registration logic and clean up imports in UserController

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,27 +1,11 @@
-import {
-  Controller,
-  Post,
-  Body,
-  Get,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
-import { UserService } from './user.service';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { User } from '../common/entities/user.entity';
 import { AuthenticatedRequest } from 'src/common/types/request.types';
 import { JwtPayload } from 'src/common/types/user.types';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post('register')
-  async register(
-    @Body() body: { username: string; password: string },
-  ): Promise<User> {
-    return this.userService.create(body);
-  }
+  constructor() {}
 
   @UseGuards(AuthGuard('jwt'))
   @Get('profile')
